31eed5ce35624538b50884112f0775dd65174597
Ashwani Gupta <ashwanigupta125@gmail.com>
2019-03-01 16:09:11 +0530

    Initial commit

---
+++ plan.md
@@ -0,0 +1,18 @@
+# Plan of action
+- basic app up (done)
+- 
+
+
+
+
+# Modules/Concepts for learning
+- argparse
+- decorators (with/without arguments)
+- time.sleep
+- win10toast, pywin32
+
+
+# Problems/Bugs/Improvements
+- unidentified lag after time.sleep()
+- make toaster decorator take duration(the time for which toast will show) arguments
+- make it an executable command line tool
---
+++ timer.py
@@ -0,0 +1,55 @@
+import time
+import argparse
+
+from win10toast import ToastNotifier
+
+APP_NAME = 'Simple Timer'
+
+
+def toast_when_finished(toast_duration, start_msg, end_msg):
+    def decorator(f):
+        toaster = ToastNotifier()
+        def wrapper(*args, **kwargs):
+            # notify when timer starts
+            toaster.show_toast(
+                title=APP_NAME,
+                msg=start_msg,
+                duration=toast_duration
+            )
+            ret_val = f(*args, **kwargs)
+            # notify after timer ends
+            toaster.show_toast(
+                title=APP_NAME,
+                msg=end_msg,
+                duration=toast_duration
+            )
+            return ret_val
+        return wrapper
+    return decorator
+
+
+@toast_when_finished(
+    toast_duration=10,
+    start_msg='Time is running. Go do your job!',
+    end_msg='Yo! your time is up lol')
+def start_timer(minutes):
+    print('That\'s it, you\'ll be notified after {} minute(s)'.format(minutes))
+    time.sleep(minutes*60)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='A utility timer alarm which notifies your Windows 10 machine when timer is finished'
+    )
+
+    parser.add_argument('duration', action='store', type=int,
+                        help='Duration of timer in minutes')
+    args = parser.parse_args()
+    start_timer(args.duration)
+    print('Yo! Your time is up!')
+
+
+if __name__ == '__main__':
+    main()
+
+
